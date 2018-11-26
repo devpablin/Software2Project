0dd24039055f2e108de8eaf16fb8c378b1116317
DevPablin <pablingonzales@gmail.com>
2018-11-26 19:08:53 -0400

    added general specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20181126155550) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,13 +23,6 @@ ActiveRecord::Schema.define(version: 20181126155550) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
-  create_table "torta", force: :cascade do |t|
-    t.string "sabor"
-    t.integer "porciones"
-    t.float "precio"
-    t.string "tipo"
 
   create_table "inventarios", force: :cascade do |t|
     t.string "nombre"
@@ -53,7 +44,15 @@ ActiveRecord::Schema.define(version: 20181126155550) do
     t.string "tipo"
     t.float "precio"
     t.date "fecha_vencimiento"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
+  create_table "torta", force: :cascade do |t|
+    t.string "sabor"
+    t.integer "porciones"
+    t.float "precio"
+    t.string "tipo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
