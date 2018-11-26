345fcd6a47e7c5970cc94ff218ac055f6c4a0fe1
DevPablin <pablingonzales@gmail.com>
2018-11-26 18:08:08 -0400

    some styling and welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :inventarios
   resources :pruebas
   resources :helados
-  root 'users#index'
+  root 'welcome#welcome'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
