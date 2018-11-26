99095e190860825d94bf405c77bff790229fecb7
DevPablin <pablingonzales@gmail.com>
2018-11-26 19:53:30 -0400

    cambio en inventario.rb

--- app/models/inventario.rb
+++ app/models/inventario.rb
@@ -1,6 +1,6 @@
 class Inventario < ApplicationRecord
 
-  validates :nombre, presence: true
+  validates :nombre, presence: false
   validates :descripcion, presence: true
   validates :tipo, presence: true
   validates :precio, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }, :numericality => {:greater_than => 0, :less_than => 1000}, presence:true
